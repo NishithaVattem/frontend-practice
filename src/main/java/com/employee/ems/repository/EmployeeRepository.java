3218030953846042c41d1fe8541adb271862228c
NishithaVattem <vijayanishithavattem2000@gmail.com>
2025-05-02 14:41:59 +0000

    Polish code and formatting

---
+++ src/main/java/com/employee/ems/repository/EmployeeRepository.java
@@ -0,0 +1,13 @@
+package com.employee.ems.repository;
+
+import com.employee.ems.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmployeeRepository extends JpaRepository<Employee,Long> {
+
+}
+
+
+// Potential improvement: Add error handling
